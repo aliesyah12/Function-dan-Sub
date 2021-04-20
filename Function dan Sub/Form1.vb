9b0a9505930bbb8f7c77d702b8653d0f0a13cfe2
Aisyah Aliesyah Binti Mohd Isor <18DDT19F1113@student.ptss.edu.my>
2021-04-20 13:04:34 +0800

    function and sub

--- Function dan Sub/Form1.vb
+++ Function dan Sub/Form1.vb
@@ -1,3 +1,42 @@
 ﻿Public Class Form1
 
+    Public Sub Maklumat()
+        lblPapar1.Text = "Aisyah Aliesyah"
+    End Sub
+
+    Public Sub Info()
+        Dim strNamaPenuh As String
+        strNamaPenuh = txt1.Text
+        lblPapar2.Text = strNamaPenuh
+    End Sub
+
+    Public Function Nama()
+        lblPapar3.Text = "Aisyah"
+        Return lblPapar1
+    End Function
+
+    Public Function Papar()
+        Dim strNamaPenuh As String
+        strNamaPenuh = txt2.Text
+        lblPapar4.Text = strNamaPenuh
+        Return lblPapar4
+    End Function
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Call Maklumat()
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Call Info()
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Call Nama()
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Call Papar()
+    End Sub
+
+
 End Class
