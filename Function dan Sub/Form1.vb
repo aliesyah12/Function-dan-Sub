81e00f1a6bfbd57c07faf2e01600002a567f33ba
Aisyah Aliesyah Binti Mohd Isor <18DDT19F1113@student.ptss.edu.my>
2021-04-21 10:13:30 +0800

    function and sub

--- Function dan Sub/Form1.vb
+++ Function dan Sub/Form1.vb
@@ -3,40 +3,30 @@
     Public Sub Maklumat()
         lblPapar1.Text = "Aisyah Aliesyah"
     End Sub
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Call Maklumat()
+    End Sub
 
-    Public Sub Info()
-        Dim strNamaPenuh As String
-        strNamaPenuh = txt1.Text
+    Public Sub Info(ByVal strNamaPenuh As String)
         lblPapar2.Text = strNamaPenuh
+    End Sub
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Call Info(txt1.Text)
     End Sub
 
     Public Function Nama()
         lblPapar3.Text = "Aisyah"
         Return lblPapar1
     End Function
-
-    Public Function Papar()
-        Dim strNamaPenuh As String
-        strNamaPenuh = txt2.Text
-        lblPapar4.Text = strNamaPenuh
-        Return lblPapar4
-    End Function
-
-    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
-        Call Maklumat()
-    End Sub
-
-    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
-        Call Info()
-    End Sub
-
     Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
         Call Nama()
     End Sub
 
+    Public Function Papar(ByVal strNamaPenuh As String)
+        lblPapar4.Text = strNamaPenuh
+    End Function
     Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        Call Papar()
+        Call Papar(txt2.Text)
     End Sub
-
 
 End Class
